0a85495c76c98a0e8405815081793e4ff1f627be
Matthew Doles <matteddied@gmail.com>
2020-03-10 16:14:02 -0400

    initial demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Card } from 'react-bootstrap';
+import CubsLogo from './images/Cubs-Logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>Est. 1876</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="TeamCard">
+        <Card.Img variant="top" src={CubsLogo} />
+        <Card.Body className="TeamCardBody">
+          <Card.Title>Chicago Cubs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
