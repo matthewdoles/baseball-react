9154e350d78bb09fa6fe2bf222b3478cf1cb78f5
Matthew Doles <matteddied@gmail.com>
2020-03-17 13:34:46 -0400

    Team Hierarchy complete list touch ups

--- src/TeamHierarchy.js
+++ src/TeamHierarchy.js
@@ -4,25 +4,31 @@ import "./TeamHierarchy.css";
 
 const TeamHierarchy = props => {
   return (
-    <React.Fragment>
-        <div key={props.id} className="TeamListItem">
-          <img src={require(`${props.logo}`)} alt="" />
-          <span className="ItemName">{props.name}</span>
-          <span className="ItemLeague">{props.league}</span>
-        </div>
-        <ul className="AffiliateList">
-          {props.affiliates.map(affiliate => (
-            <div
-              key={affiliate.id}
-              className={`TeamListItem AffiliateItem ${affiliate.league.replace('+', '')}`}
-            >
-              <img src={require(`./images/${affiliate.photo}`)} alt="" />
-              <span className="ItemName">{affiliate.name}</span>
-              <span className="ItemLeague">{affiliate.league}</span>
-            </div>
-          ))}
-        </ul>
-    </React.Fragment>
+    <div className="TeamHierarchy">
+      <p className="TeamListItemTitle">
+        <span>{props.name}</span>
+      </p>
+      <div key={props.id} className="TeamListItem">
+        <img src={require(`${props.logo}`)} alt="" />
+        <span className="ItemName">{props.name}</span>
+        <span className="ItemLeague">{props.league}</span>
+      </div>
+      <ul className="AffiliateList">
+        {props.affiliates.map(affiliate => (
+          <div
+            key={affiliate.id}
+            className={`TeamListItem AffiliateItem ${affiliate.league.replace(
+              "+",
+              ""
+            )}`}
+          >
+            <img src={require(`./images/${affiliate.photo}`)} alt="" />
+            <span className="ItemName">{affiliate.name}</span>
+            <span className="ItemLeague">{affiliate.league}</span>
+          </div>
+        ))}
+      </ul>
+    </div>
   );
 };
 
