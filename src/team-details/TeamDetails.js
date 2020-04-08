839a3bf60bd6b40302e0dc4f98fc8020a38a019a
Matthew Doles <matteddied@gmail.com>
2020-04-08 17:05:13 -0400

    TeamDetails Navigation refactor

--- src/team-details/TeamDetails.js
+++ src/team-details/TeamDetails.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import Map from '../shared/Map';
-import { Card, Navbar, Nav, Modal, Button } from 'react-bootstrap';
+import Navigation from '../shared/Navigation';
+import { Card, Modal, Button } from 'react-bootstrap';
 import { useHttpClient } from '../hooks/http-hook';
 import './TeamDetails.css';
 
 const TeamDetails = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [allTeams, setAllTeams] = useState(null);
   const [selectedTeam, setSelectedTeam] = useState(null);
   const [errorModal, setErrorModal] = useState(false);
-
+  const history = useHistory();
   const teamName = useParams().team;
 
   const showErrorModal = () => setErrorModal(true);
@@ -23,6 +25,7 @@ const TeamDetails = () => {
       } catch (error) {}
       sessionStorage.setItem('teams', JSON.stringify(responseData));
     }
+    setAllTeams(responseData.teams);
     const team = responseData.teams.find((team) => team.url === teamName);
     if (team === undefined) {
       showErrorModal(true);
@@ -34,6 +37,22 @@ const TeamDetails = () => {
   useEffect(() => {
     fetchTeam();
   }, [fetchTeam]);
+
+  const findPrevTeam = () => {
+    const currentIndex = allTeams.findIndex(
+      (team) => team.id === selectedTeam.id
+    );
+    const previousTeam = allTeams[currentIndex - 1];
+    history.push(`/team/${previousTeam.url}`);
+  };
+
+  const findNextTeam = () => {
+    const currentIndex = allTeams.findIndex(
+      (team) => team.id === selectedTeam.id
+    );
+    const nextTeam = allTeams[currentIndex + 1];
+    history.push(`/team/${nextTeam.url}`);
+  };
 
   return (
     <React.Fragment>
@@ -50,101 +69,106 @@ const TeamDetails = () => {
       </Modal>
       {!isLoading && selectedTeam !== null && (
         <React.Fragment>
-          <Navbar
-            style={{
-              backgroundColor: selectedTeam.photoColor,
-            }}
-          >
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Link
-                  href='/'
-                  style={{
-                    color: 'white',
-                    fontSize: '20px',
-                  }}
-                >
-                  Back
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+          <Navigation />
           <div className='Container'>
-            <div className='C1'>
-              <Card className='TeamDetailCard'>
-                <Card.Img
-                  variant='top'
-                  className='TeamLogoImage'
-                  src={require(`../images/${selectedTeam.photo}`)}
-                  style={{
-                    backgroundColor: selectedTeam.color,
-                  }}
-                />
-              </Card>
+            <div
+              style={{
+                backgroundColor: selectedTeam.photoColor,
+                height: '50px',
+                display: 'flex',
+                margin: 'auto',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '1rem',
+              }}
+            >
+              <p className='TeamDetailsNavigationItem' onClick={findPrevTeam}>
+                PREVIOUS
+              </p>
+              <p className='TeamDetailsNavigationItem'>|</p>
+              <p className='TeamDetailsNavigationItem'>RANDOM</p>
+              <p className='TeamDetailsNavigationItem'>|</p>
+              <p className='TeamDetailsNavigationItem' onClick={findNextTeam}>
+                NEXT
+              </p>
             </div>
-            <div className='C2'>
-              <Card className='TeamDetailCard'>
-                <Card.Header
-                  className='TeamDetailCardHeader'
-                  style={{
-                    backgroundColor: selectedTeam.photoColor,
-                  }}
-                >
-                  {selectedTeam.name}
-                </Card.Header>
-                <Card.Body>
-                  <div className='DetailsBody'>
-                    <div className='DetailsLeft'>
-                      <p>
-                        <b>Established:</b> {selectedTeam.established}
-                      </p>
-                      <p>
-                        <b>Stadium:</b> {selectedTeam.stadium}
-                      </p>
-                      <p>
-                        <b>Capacity:</b>{' '}
-                        {selectedTeam.capacity.toLocaleString('en')}
-                      </p>
-                      <p>
-                        <b>Address:</b> {selectedTeam.address}
-                      </p>
-                    </div>
-                    <div className='DetailsRight'>
-                      <p>
-                        <b>Organization:</b> {selectedTeam.league}
-                      </p>
-                      <p>
-                        <b>League:</b> {selectedTeam.conference}
-                      </p>
-                      <p>
-                        <b>Divison:</b> {selectedTeam.division}
-                      </p>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className='C3'>
-              <Card className='TeamDetailCard'>
-                <Card.Header
-                  className='TeamDetailCardHeader'
-                  style={{
-                    backgroundColor: selectedTeam.photoColor,
-                    color: 'white',
-                    fontSize: '20px',
-                  }}
-                >
-                  {selectedTeam.stadium}
-                </Card.Header>
-                <Card.Body>
-                  <Map
-                    lat={selectedTeam.location.latitude}
-                    lng={selectedTeam.location.longitude}
-                    zoom={14}
-                    style={{ width: '100%', height: '100%' }}
+            <div className='TeamDetails'>
+              <div className='C1'>
+                <Card className='TeamDetailCard'>
+                  <Card.Img
+                    variant='top'
+                    className='TeamLogoImage'
+                    src={require(`../images/${selectedTeam.photo}`)}
+                    style={{
+                      backgroundColor: selectedTeam.color,
+                    }}
                   />
-                </Card.Body>
-              </Card>
+                </Card>
+              </div>
+              <div className='C2'>
+                <Card className='TeamDetailCard'>
+                  <Card.Header
+                    className='TeamDetailCardHeader'
+                    style={{
+                      backgroundColor: selectedTeam.photoColor,
+                    }}
+                  >
+                    {selectedTeam.name}
+                  </Card.Header>
+                  <Card.Body>
+                    <div className='DetailsBody'>
+                      <div className='DetailsLeft'>
+                        <p>
+                          <b>Established:</b> {selectedTeam.established}
+                        </p>
+                        <p>
+                          <b>Stadium:</b> {selectedTeam.stadium}
+                        </p>
+                        <p>
+                          <b>Capacity:</b>{' '}
+                          {selectedTeam.capacity.toLocaleString('en')}
+                        </p>
+                        <p>
+                          <b>Address:</b> {selectedTeam.address}
+                        </p>
+                      </div>
+                      <div className='DetailsRight'>
+                        <p>
+                          <b>Organization:</b> {selectedTeam.league}
+                        </p>
+                        <p>
+                          <b>League:</b> {selectedTeam.conference}
+                        </p>
+                        <p>
+                          <b>Divison:</b> {selectedTeam.division}
+                        </p>
+                      </div>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className='C3'>
+                <Card className='TeamDetailCard'>
+                  <Card.Header
+                    className='TeamDetailCardHeader'
+                    style={{
+                      backgroundColor: selectedTeam.photoColor,
+                      color: 'white',
+                      fontSize: '20px',
+                    }}
+                  >
+                    {selectedTeam.stadium}
+                  </Card.Header>
+                  <Card.Body>
+                    <Map
+                      lat={selectedTeam.location.latitude}
+                      lng={selectedTeam.location.longitude}
+                      zoom={14}
+                      style={{ width: '100%', height: '100%' }}
+                    />
+                  </Card.Body>
+                </Card>
+              </div>
             </div>
           </div>
         </React.Fragment>
