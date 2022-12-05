cbe66864052a0341a05c0d0346344e70c5c14cac
Matthew Doles <matteddied@gmail.com>
2022-12-05 08:42:17 -0600

    Remove firebase config and workflow

--- src/__test__/AllTeams.test.js
+++ src/__test__/AllTeams.test.js
@@ -9,37 +9,70 @@ import {
 } from '@testing-library/react';
 
 import AllTeams from 'pages/AllTeams';
+import { mount } from 'enzyme';
 
-global.fetch = jest.fn().mockImplementation(() =>
+global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () =>
       Promise.resolve({
-        teams: [
-          {
-            id: 'a0I1U00000FfwjzUAB',
-            name: 'Aberdeen IronBirds',
-            url: 'AberdeenIronBirds',
-            established: 1977,
-            photo: 'Orioles/IronBirds-Logo.jpg',
-            photoColor: '#0376b8',
-            league: 'SS',
-            conference: 'New York–Penn League',
-            division: 'McNamara',
-            stadium: 'Leidos Field at Ripken Stadium',
-            address: '873 Long Dr, Aberdeen, MD 21001',
-            location: {
-              latitude: 39.531,
-              longitude: -76.1867,
-            },
-            capacity: 6300,
-          },
-        ],
+        id: 'a0I1U00000FfwjzUAB',
+        name: 'Aberdeen IronBirds',
+        url: 'AberdeenIronBirds',
+        established: 1977,
+        photo: 'Orioles/IronBirds-Logo.jpg',
+        photoColor: '#0376b8',
+        league: 'SS',
+        conference: 'New York–Penn League',
+        division: 'McNamara',
+        stadium: 'Leidos Field at Ripken Stadium',
+        address: '873 Long Dr, Aberdeen, MD 21001',
+        location: {
+          latitude: 39.531,
+          longitude: -76.1867,
+        },
+        capacity: 6300,
       }),
   }),
 );
 
 it('opens navigation disclaimer modal then closes', async () => {
-  render(<AllTeams />);
+  const mResponse = jest.fn().mockResolvedValue({
+    teams: [
+      {
+        id: 'a0I1U00000FfwjzUAB',
+        name: 'Aberdeen IronBirds',
+        url: 'AberdeenIronBirds',
+        established: 1977,
+        photo: 'Orioles/IronBirds-Logo.jpg',
+        photoColor: '#0376b8',
+        league: 'SS',
+        conference: 'New York–Penn League',
+        division: 'McNamara',
+        stadium: 'Leidos Field at Ripken Stadium',
+        address: '873 Long Dr, Aberdeen, MD 21001',
+        location: {
+          latitude: 39.531,
+          longitude: -76.1867,
+        },
+        capacity: 6300,
+      },
+    ],
+  });
+  global.fetch = jest.fn(() => {
+    return Promise.resolve({ json: mResponse });
+  });
+  const wrapper = mount(<AllTeams />);
+  expect(wrapper.exists()).toBeTruthy();
+
+  //expect(wrapper.text()).toBe('country: , cntCode: ');
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  });
+  console.log(wrapper.debug());
+  //expect(wrapper.text()).toBe('country: US, cntCode: 123');
+  //expect(global.fetch).toBeCalledWith('http://ip-api.com/json');
+
+  //await act(async () => render(<AllTeams />));
   //console.log(screen.debug());
   //console.log((await global.fetch()).json());
   //await waitForElement(() => screen.getByText('Aberdeen'));
